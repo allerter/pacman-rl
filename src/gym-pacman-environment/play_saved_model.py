87cfdf23baea55781a986bef6f69ef16c6e0cc14
allerter <a.hazhir@gmail.com>
2025-01-17 09:34:16 +0100

    fix detecting level when playing from model

--- src/gym-pacman-environment/play_saved_model.py
+++ src/gym-pacman-environment/play_saved_model.py
@@ -1,4 +1,5 @@
 import os
+import re
 import configparser
 import PacmanAgent
 from dyna_q import DynaQ
@@ -8,10 +9,13 @@ from utils import get_level
 
 if __name__ == "__main__":
 
-    filename = "" # set this to the file path of the pickled model
+    filename = "models/DynaQ/RL02_square_tunnel_H/DynaQ - RL02_square_tunnel_H E=3000 LR=0.2 DF=0.2 EP=0.13070194999971538 SS=0.pkl" # set this to the file path of the pickled model
 
     env = PacmanAgent.PacmanAgent()
-    if filename.lower().startswith("dynaq"):
+    PacmanAgent.level_name = match = re.search(r"(?:DynaQ|DQN)\s+-\s+(RL\S+)", filename).group(1)
+    print(PacmanAgent.level_name)
+    PacmanAgent.level = get_level(PacmanAgent.level_name)
+    if "dynaq" in filename.lower():
         model = DynaQ.load(env, filename)
     else:
         model = DQN.load(env, filename)
